b547817db61d7eb5b1256faa596fe4396e58b87b
DevCollynx <clementcollins987@gmail.com>
2026-01-17 18:39:36 +0100

    Responsiveness

--- src/components/student/SearchBar.jsx
+++ src/components/student/SearchBar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const SearchBar = () => {
   return (
     <div>
-      <h1>SearchBar</h1>
+      <form
+        className="max-w-xl w-50 md:h-12 h-10 flex items-center 
+      bg-white border border-gray-500/20 rounded"
+      >
+        <img
+          src={assets.search_icon}
+          alt="search_icon"
+          className="md:w-auto w-10 px-3"
+        />
+        <input
+          type="text"
+          placeholder="Search for courses"
+          className="w-full h-full outline-none text-gray-500/80"
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 rounded text-white 
+        md:px-10 px-6 md:py-3 py-2 ml-50"
+        >
+          Search
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
