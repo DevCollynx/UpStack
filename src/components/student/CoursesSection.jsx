078bdfe8a8a4d6c8739130fc875172e4f28e4284
DevCollynx <clementcollins987@gmail.com>
2026-01-19 17:43:15 +0100

    Added Courses

--- src/components/student/CoursesSection.jsx
+++ src/components/student/CoursesSection.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import CourseCard from './CourseCard'
 
 const CoursesSection = () => {
+
+  const {allCourses} = useContext(AppContext)
+
   return (
     <div className="py-16 md:px-40 px-8">
       <h2 className="text-3xl font-medium text-gray-800">
@@ -11,6 +15,10 @@ const CoursesSection = () => {
         and design to business and wellness, our courses are crafted to deliver
         results.
       </p>
+
+      <div>
+        {allCourses.slice(0,4).map((course, index)=> <CourseCard key= {index} course={course}/>)}
+      </div>
 
       <Link
         to={"/course-list"}
