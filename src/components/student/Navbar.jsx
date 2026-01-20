4258928506f79754153dc9f5545d21149868c89e
DevCollynx <clementcollins987@gmail.com>
2026-01-20 18:26:54 +0100

    RoutePath to Educator Dashboard

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
+
+  const {navigate, isEducator} = useContext(AppContext)
+
   const isCourseListPage = location.pathname.includes("/course-list");
 
   const { openSignIn } = useClerk();
@@ -16,17 +20,25 @@ const Navbar = () => {
       }`}
     >
       <img
+        onClick={() => navigate("/")}
         src={assets.logo}
         alt="Logo"
         className="w-28 lg:w-32 cursor-pointer"
       />
       <div className="hidden md:flex items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5">
-          {user && <>
-            <button>Become Educator</button>
-          <Link to="/my-enrollments">My Enrollments</Link>
-          </>
-            }
+          {user && (
+            <>
+              <button
+                onClick={() => {
+                  navigate("/educator");
+                }}
+              >
+                {isEducator ? "Educator Dashboard" : "Become Educator"}
+              </button>
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
         </div>
         {user ? (
           <UserButton />
@@ -43,18 +55,26 @@ const Navbar = () => {
       {/* For Phone Screen */}
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
         <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
-          {user && <>
-            <button>Become Educator</button>
-          <Link to="/my-enrollments">My Enrollments</Link>
-          </>
-            }
+          {user && (
+            <>
+              <button
+                onClick={() => {
+                  navigate("/educator");
+                }}
+              >
+                {isEducator ? "Educator Dashboard" : "Become Educator"}
+              </button>
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
         </div>
-        {
-          user ? <UserButton/> : <button onClick={()=> openSignIn()}>
-          <img src={assets.user_icon} alt="" />
-        </button>
-        }
-        
+        {user ? (
+          <UserButton />
+        ) : (
+          <button onClick={() => openSignIn()}>
+            <img src={assets.user_icon} alt="" />
+          </button>
+        )}
       </div>
     </div>
   );
