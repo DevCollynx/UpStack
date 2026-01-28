e7826c1d726f66376c72a01d05fb73661a69531f
DevCollynx <clementcollins987@gmail.com>
2026-01-28 13:16:43 +0100

    Defined the useContext and Link tag

--- src/components/student/CourseCard.jsx
+++ src/components/student/CourseCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { AppContext } from "../../context/AppContext";
 import { Link } from "react-router-dom";
@@ -8,14 +8,14 @@ const CourseCard = ({ course }) => {
 
   return (
     <Link
-      to={"/course/" + course._id}
+      to={"/course/" + `course._id`}
       onClick={() => scrollTo(0, 0)}
       className="border border-gray-500/30
     pb-6 overflow-hidden rounded-lg"
     >
-      <img className="w-full" src={course.courseThumbnail} alt="" />
+      {/* <img className="w-full" src={course.courseThumbnail} alt="" /> */}
       <div className="p-3 text-left">
-        <h3 className="text-base font-semibold">{course.courseTitle}</h3>
+        {/* <h3 className="text-base font-semibold">{course.courseTitle}</h3> */}
         <p className="text-gray-500">UpStack</p>
         <div className="flex items-center space-x-2">
           <p>{calculateRating(course)}</p>
