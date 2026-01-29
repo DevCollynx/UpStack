8997e012bfd9c48e39adf37d660729d396126761
DevCollynx <clementcollins987@gmail.com>
2026-01-29 20:07:50 +0100

    Made it responsive

--- src/components/student/TestimonialSection.jsx
+++ src/components/student/TestimonialSection.jsx
@@ -4,13 +4,13 @@ import { assets, dummyTestimonial } from "../../assets/assets";
 const TestimonialSection = () => {
   return (
     <div className="pb-14 px-8 md:px-0">
-      <h2 className="text-8xl font-medium text-gray-800">Testimonials</h2>
+      <h2 className="text-3xl font-medium text-gray-800">Testimonials</h2>
       <p className="md:text-base text-gray-500 mt-3">
         Hear from our learners as they share their journey of transformation,
         success, and how our <br /> platform has made a difference in their
         lives.
       </p>
-      <div className="grid grid-cols-auto gap-8 mt-14">
+      <div className="flex flex-col md:flex-row lg:mx-75 mx-5 gap-8 mt-14">
         {dummyTestimonial.map((testimonial, index) => (
           <div
             key={index}
