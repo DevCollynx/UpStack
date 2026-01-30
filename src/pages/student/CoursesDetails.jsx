84ebeeeebf80dc71e2cded2f954d323c8a4637d7
DevCollynx <clementcollins987@gmail.com>
2026-01-30 22:14:37 +0100

    Added new updates

--- src/pages/student/CoursesDetails.jsx
+++ src/pages/student/CoursesDetails.jsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/student/Loading";
 import { assets } from "../../assets/assets";
+import humanizeDuration from "humanize-duration";
 
 const CoursesDetails = () => {
   const { id } = useParams();
 
   const [courseData, setCourseData] = useState(null);
+  const [openSections, setOpenSections] = useState({});
 
   const {
     allCourses,
@@ -15,6 +17,7 @@ const CoursesDetails = () => {
     calculateChapterTime,
     calculateCourseDuration,
     calculateNoOfLectures,
+    currency,
   } = useContext(AppContext);
 
   const fetchCourseData = async () => {
@@ -25,6 +28,14 @@ const CoursesDetails = () => {
   useEffect(() => {
     fetchCourseData();
   }, []);
+
+  const toggleSection = (index)=> {
+    setOpenSections((prev)=>(
+      {...prev,
+        [index]: !prev[index],
+      }
+    ))
+  }
   
   return courseData ? (
     <>
@@ -33,13 +44,13 @@ const CoursesDetails = () => {
       justify-between md:px-20 px-15 md:pt-30 pt-20 text-left"
       >
         <div
-          className="absolute top-0 left-0 w-full
+          className="absolute top-0 left-0 w-full section-height
          -z-1 h-full bg-linear-gradient-to-b from-cyan-100 to-blue-70"
         ></div>
 
         {/* left column  */}
         <div className="max-w-xl z-10 text-gray-500">
-          <h1 className="md:text-[48px]/[56px] text-[28px]/[34px] font-semibold text-gray-800">
+          <h1 className="md:text-[48px]/[56px] text-[28px]/[34px]  font-semibold text-gray-800">
             {courseData.courseTitle}
           </h1>
           <p
@@ -81,40 +92,103 @@ const CoursesDetails = () => {
             Course by <span className="text-blue-600 underline">UpStack</span>
           </p>
           <div className="pt-8 text-gray-800">
-                <h2 className="text-xl font-semibold ">Course Structure</h2>
-                <div className="pt-5">
-                  {courseData.courseContent.map((chapter, index)=> (
-                    <div key={index} className="border border-gray-300 bg-white mb-2 rounded">
-                      <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
-                        <div className="flex items-center gap-2">
-                          <img src={assets.down_arrow_icon} alt="arrow icon" />
-                          <p className="font-medium md:text-base text-sm">{chapter.chapterTitle}</p>
-                        </div>
-                        <p className="text-sm md:text-default">{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
-                      </div>
-                      <div>
-                        <ul>
-                          {chapter.chapterContent.map((lecture, index)=> (
-                            <li key={i}>
-                              <img src={assets.play_icon} alt="play_icon" className="w-4 h-4 mt-1" />
-                              <div>
-                                <p>{lecture.lectureTitle}</p>
-                                <div>
-                                  {lecture.i}
-                                </div>
-                              </div>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
+            <h2 className="text-xl font-semibold ">Course Structure</h2>
+            <div className="pt-5">
+              {courseData.courseContent.map((chapter, index) => (
+                <div
+                  key={index}
+                  className="border border-gray-300 bg-white mb-2 rounded"
+                >
+                  <div
+                    className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
+                    onClick={() => toggleSection(index)}
+                  >
+                    <div className="flex items-center gap-2">
+                      <img
+                        className={`transform transition-transform ${openSections[index] ? "rotate-180" : ""}`}
+                        src={assets.down_arrow_icon}
+                        alt="arrow icon"
+                      />
+                      <p className="font-medium md:text-base text-sm">
+                        {chapter.chapterTitle}
+                      </p>
                     </div>
-                  ))}
+                    <p className="text-sm md:text-default">
+                      {chapter.chapterContent.length} lectures -{" "}
+                      {calculateChapterTime(chapter)}
+                    </p>
+                  </div>
+
+                  <div
+                    className={`overflow-hidden transition-all duration-300 ${openSections[index] ? "max-h-96" : "max-h-0"}`}
+                  >
+                    <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
+                      {chapter.chapterContent.map((lecture, i) => (
+                        <li key={i} className="flex items-start gap-2 py-1">
+                          <img
+                            src={assets.play_icon}
+                            alt="play icon"
+                            className="w-4 h-4 mt-1"
+                          />
+                          <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
+                            <p>{lecture.lectureTitle}</p>
+                            <div className="flex gap-2">
+                              {lecture.isPreviewFree && (
+                                <p className="text-blue-500 cursor-pointer">
+                                  Preview
+                                </p>
+                              )}
+                              <p>
+                                {humanizeDuration(
+                                  lecture.lectureDuration * 60 * 1000,
+                                  { units: ["h", "m"] },
+                                )}
+                              </p>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="py-20 text-sm md:text-default">
+            <h3 className="text-xl font-semibold text-gray-800">
+              Course Description
+            </h3>
+            <p
+              className="pt-3 rich-text"
+              dangerouslySetInnerHTML={{
+                __html: courseData.courseDescription,
+              }}
+            ></p>
           </div>
         </div>
 
         {/* right column */}
-        <div></div>
+        <div className="max-w-[424px] z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
+          <img src={courseData.courseThumbnail} alt="" />
+          <div className="p-5">
+            <div className="flex items-center gap-2">
+              <img
+                className="w-3.5"
+                src={assets.time_left_clock_icon}
+                alt="time left clock icon"
+              />
+              <p className="text-red-500">
+                <span className="font-medium">5 days</span> left at this price!
+              </p>
+            </div>
+
+            <div>
+              <p className="md:text-lg text-gray-500 line-through">{currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}</p>
+              <p className="md:text-lg text-gray-500">{courseData.discount}% off</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   ) : (
