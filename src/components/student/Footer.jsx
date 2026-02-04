da3cd448095ebd8eb7da5104438fa3dd3e3343d9
DevCollynx <clementcollins987@gmail.com>
2026-02-04 19:39:54 +0100

    Fixed the logo

--- src/components/student/Footer.jsx
+++ src/components/student/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       justify-center gap-10 md:gap-32 py-10 border-b border-white/30"
       >
         <div className="flex flex-col md:items-start items-center w-full">
-          <img src={assets.logo_dark} alt="logo" />
+          <img src={assets.upstack_logo_light} alt="logo" />
           <p className="mt-6 text-center md:text-left text-sm text-white/80">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text.
@@ -36,11 +36,19 @@ const Footer = () => {
           <h2 className="font-semibold text-white mb-5">
             Subscribe to our newsletter
           </h2>
-          <p className="text-sm text-white/80">The latest news, articles, and resources, sent to your inbox weekly.</p>
+          <p className="text-sm text-white/80">
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
           <div className="flex items-center gap-2 pt-4">
-            <input type="email" placeholder="Enter your email" className="border border-gray-500/30
-            bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm" />
-            <button className="bg-blue-600 w-24 h-9 text-white rounded">Subscribe</button>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="border border-gray-500/30
+            bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm"
+            />
+            <button className="bg-blue-600 w-24 h-9 text-white rounded">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
