a212dffd52a08df01dabff094ed1187100a08c3b
DevCollynx <clementcollins987@gmail.com>
2026-02-05 07:03:16 +0100

    made new changes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg'
 import logo_dark from './logo_dark.svg'
 import upstack from './upstack.svg'
-import upstack_logo_light from './upstack_logo_light.svg'
-import upstack_logo_dark from './upstack_logo_dark.svg'
+import light from './light.svg'
+import dark from './dark.svg'
 import search_icon from './search_icon.svg'
 import cross_icon from './cross_icon.svg'
 import upload_area from './upload_area.svg'
@@ -52,8 +52,8 @@ export const assets = {
     search_icon,
     sketch,
     upstack,
-    upstack_logo_dark,
-    upstack_logo_light,
+    dark,
+    light,
     microsoft_logo,
     walmart_logo,
     accenture_logo,
